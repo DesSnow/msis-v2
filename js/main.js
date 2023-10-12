8a9ea8ec51e811e98e936acd8ed8d8142108b9ed
Tom <97752359+DesSnow@users.noreply.github.com>
2023-10-12 17:12:08 +0000

    discordcount

--- js/main.js
+++ js/main.js
@@ -11,6 +11,20 @@
     };
     spinner();
     
+    //Discord_api
+    var discord_key = "716689048323751996";
+    var xmlHttpRequest = new XMLHttpRequest();
+        xmlHttpRequest.open("GET", "https://discord.com/api/guilds/" + discord_key + "/embed.json", true);
+        xmlHttpRequest.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                var d = JSON.parse(this.responseText);
+                document.getElementById("discordCount").innerHTML = d.presence_count;
+            }  
+        };  
+   xmlHttpRequest.send();  
+   
+
+
     
     // Initiate the wowjs
     new WOW().init();
